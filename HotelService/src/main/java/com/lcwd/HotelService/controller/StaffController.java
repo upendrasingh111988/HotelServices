2f1b08bf8b2fd6c4a5228f3db47b755ae4b82180
upendra singh <upendra.singh111988@gmail.com>
2024-08-26 16:05:01 +0530

    okta

---
+++ HotelService/src/main/java/com/lcwd/HotelService/controller/StaffController.java
@@ -0,0 +1,22 @@
+package com.lcwd.HotelService.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+	
+	@GetMapping("")
+	public ResponseEntity<List<String>> getStaff(){
+		List<String>list= Arrays.asList("Upendra","Neha","Vashu","Soumya");
+		return new ResponseEntity<List<String>>(list,HttpStatus.OK);
+	}
+
+}
